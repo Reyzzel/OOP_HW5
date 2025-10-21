ce4050ae5e67f606699aed8016c833837e9886fe
Reyzzel <demc.valeria@gmail.com>
2025-10-21 11:03:45 +0300

    Add files via upload

---
+++ structures_and_classes/structures_and_classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace structures_and_classes
+{
+    struct Student
+    {
+        int age;
+        string name;
+
+        // Цей конструктор буде викликано
+        public Student()
+        {
+            age = 16;
+            name = "Ivan";
+        }
+
+        // Додамо метод, щоб побачити результат
+        public void Print()
+        {
+            Console.WriteLine($"Ім'я: {name}, Вік: {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Student st = new Student();
+
+            st.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
